f0ec353b3698dacc6696810242f644f3aa5a3779
worksoup <2055038075@qq.com>
2024-02-06 20:20:56 +0800

    优化宏的适用范围。

--- crates/mj_macro/src/lib.rs
+++ crates/mj_macro/src/lib.rs
@@ -3,18 +3,53 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn;
-use syn::{Data, Field, Fields, Type};
-
+use syn::{Attribute, Data, DeriveInput, Field, Fields, Meta, Type};
+fn impl_get_as(
+    ast_data: &Data,
+    name: &proc_macro2::Ident,
+    struct_impl: proc_macro2::TokenStream,
+    fn_name: proc_macro2::TokenStream,
+) -> proc_macro2::TokenStream {
+    match &ast_data {
+        Data::Struct(_) => struct_impl,
+        Data::Enum(data_enum) => {
+            let variants = &data_enum.variants;
+            let tokens = variants.iter().map(|variant| {
+                let ident = &variant.ident;
+                quote!(
+                    #name::#ident(a) => a.#fn_name(),
+                )
+            });
+            quote!(
+                match self {
+                    #(#tokens)*
+                }
+            )
+        }
+        Data::Union(_) => {
+            panic!("不支持使用 `union`!")
+        }
+    }
+}
 #[proc_macro_derive(GetInstanceDerive)]
 pub fn get_instance_derive(input: TokenStream) -> TokenStream {
-    let ast: &syn::DeriveInput = &syn::parse(input).unwrap();
+    let ast: &DeriveInput = &syn::parse(input).unwrap();
     let name = &ast.ident;
     let generics = &ast.generics;
+    let r#impl = impl_get_as(
+        &ast.data,
+        name,
+        quote!(j4rs::Jvm::attach_thread()
+            .unwrap()
+            .clone_instance(&self.instance)
+            .unwrap()),
+        quote!(get_instance),
+    );
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
     let gen = quote! {
         impl #impl_generics mj_base::env::GetInstanceTrait for #name #ty_generics #where_clause {
             fn get_instance(&self) -> j4rs::Instance{
-                j4rs::Jvm::attach_thread().unwrap().clone_instance(&self.instance).unwrap()
+                #r#impl
             }
         }
     };
@@ -22,20 +57,21 @@ pub fn get_instance_derive(input: TokenStream) -> TokenStream {
 }
 #[proc_macro_derive(AsInstanceDerive)]
 pub fn as_instance_derive(input: TokenStream) -> TokenStream {
-    let ast: &syn::DeriveInput = &syn::parse(input).unwrap();
+    let ast: &DeriveInput = &syn::parse(input).unwrap();
     let name = &ast.ident;
     let generics = &ast.generics;
+    let r#impl = impl_get_as(&ast.data, name, quote!(&self.instance), quote!(as_instance));
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
     let gen = quote! {
         impl #impl_generics mj_base::env::AsInstanceTrait for #name #ty_generics #where_clause {
             fn as_instance(&self) -> &j4rs::Instance{
-                &self.instance
+                #r#impl
             }
         }
     };
     gen.into()
 }
-#[proc_macro_derive(FromInstanceDerive)]
+#[proc_macro_derive(FromInstanceDerive, attributes(fall))]
 pub fn from_instance_derive(input: TokenStream) -> TokenStream {
     fn type_is_phantom_data(field: &Field) -> bool {
         if let Type::Path(ref ty) = field.ty {
@@ -57,21 +93,76 @@ pub fn from_instance_derive(input: TokenStream) -> TokenStream {
         }
         tokens
     }
-    let ast: &syn::DeriveInput = &syn::parse(input).unwrap();
-    let filled_fields = match &ast.data {
-        Data::Struct(s) => fill_phantom_data_fields(&s.fields),
-        _ => proc_macro2::TokenStream::new(),
-    };
+    let ast: &DeriveInput = &syn::parse(input).unwrap();
     let name = &ast.ident;
     let generics = &ast.generics;
+    let impl_tokens = match &ast.data {
+        Data::Struct(s) => {
+            let tmp = fill_phantom_data_fields(&s.fields);
+            quote!(
+                Self{
+                        instance,
+                        #tmp
+                    }
+            )
+        }
+        Data::Enum(e) => {
+            let variants = &e.variants;
+            let mut fall_arm = variants.first();
+            let mut impl_tokens = proc_macro2::TokenStream::new();
+            for variant in variants {
+                let has_this_attr = if let Some(field_attr) = variant.attrs.first() {
+                    if let Some(ident) = field_attr.path().get_ident() {
+                        ident == "fall"
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                };
+                if has_this_attr {
+                    fall_arm = Some(&variant);
+                } else {
+                    let ty = match &variant.fields {
+                        Fields::Unnamed(fields) => {
+                            &fields.unnamed.first().expect("无名枚举没有字段！").ty
+                        }
+                        _ => {
+                            panic!("不支持无内含值的枚举以及有名枚举！")
+                        }
+                    };
+                    let ident = &variant.ident;
+                    impl_tokens.extend(quote!(
+                        if <#ty as mj_base::env::GetClassTypeTrait>::is_this_type(&instance) {
+                            #name::#ident(
+                                #ty::from_instance(
+                                    <#ty as mj_base::env::GetClassTypeTrait>::cast_to_this_type(instance)
+                                )
+                            )
+                        } else
+                    ))
+                }
+            }
+            let fall_arm = fall_arm.expect("须有 `fall` 属性！");
+            let fall_arm_ty = match &fall_arm.fields {
+                Fields::Unnamed(fields) => &fields.unnamed.first().expect("无名枚举没有字段！").ty,
+                _ => {
+                    panic!("不支持无内含值的枚举以及有名枚举！")
+                }
+            };
+            let fall_arm_ident = &fall_arm.ident;
+            impl_tokens.extend(quote!(
+                {#name::#fall_arm_ident(<#fall_arm_ty as mj_base::env::FromInstance>::from_instance(instance))}
+            ));
+            impl_tokens
+        }
+        Data::Union(_) => panic!("不支持使用 `union`!"),
+    };
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
     let gen = quote! {
         impl #impl_generics mj_base::env::FromInstance for #name #ty_generics #where_clause {
             fn from_instance(instance: j4rs::Instance) -> Self{
-                Self{
-                    instance,
-                    #filled_fields
-                }
+                #impl_tokens
             }
         }
     };
@@ -81,7 +172,7 @@ pub fn from_instance_derive(input: TokenStream) -> TokenStream {
 /// 不要求结构体有 `instance: Instance` 字段。
 #[proc_macro_attribute]
 pub fn java_type(type_name: TokenStream, input: TokenStream) -> TokenStream {
-    let ast: &syn::DeriveInput = &syn::parse(input).unwrap();
+    let ast: &DeriveInput = &syn::parse(input).unwrap();
     let type_name: &syn::LitStr = &syn::parse(type_name).expect("类型名称请用字符串表示！");
     let name = &ast.ident;
     let generics = &ast.generics;

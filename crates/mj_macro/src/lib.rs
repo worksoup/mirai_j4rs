af7fa121c96b03306d97128e136bfa2aaab13519
worksoup <2055038075@qq.com>
2024-02-07 14:33:02 +0800

    将 mj_macro 的文档转移到注释当中。

--- crates/mj_macro/src/lib.rs
+++ crates/mj_macro/src/lib.rs
@@ -1,5 +1,4 @@
-extern crate proc_macro;
-
+//! 这里存放了开发 `mirai_j4rs` 时用到的一些宏。
 use proc_macro::TokenStream;
 
 use proc_macro2::Span;
@@ -44,6 +43,24 @@ fn add_prefix(input: TokenStream) -> LitStr {
         Span::mixed_site(),
     )
 }
+
+/// ### `GetInstanceDerive`
+///
+/// 为特定的结构体和枚举类型实现 [`GetInstanceTrait`](mj_base::env::GetInstanceTrait).
+///
+/// 这些类型需要满足如下条件：
+///
+/// - 结构体必须拥有 `instance: j4rs::Instance,` 字段。
+/// - 枚举值则必须类似于此：
+///   ```rust
+///   struct AType;
+///   struct BType;
+///   enum Enum{
+///     A(AType),
+///     B(BType),
+///   }
+///   ```
+///   并且如上代码，`AType` 和 `BType` 都必须实现 `GetInstanceTrait`.
 #[proc_macro_derive(GetInstanceDerive)]
 pub fn get_instance_derive(input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
@@ -68,6 +85,10 @@ pub fn get_instance_derive(input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
+
+/// ### `AsInstanceDerive`
+///
+/// 与 [`GetInstanceDerive`] 类似。
 #[proc_macro_derive(AsInstanceDerive)]
 pub fn as_instance_derive(input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
@@ -84,6 +105,27 @@ pub fn as_instance_derive(input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
+/// ### `FromInstanceDerive`
+///
+/// 为特定的结构体和枚举类型实现 [`FromInstanceTrait`](mj_base::env::FromInstanceTrait).
+///
+/// 这些类型需要满足如下条件：
+///
+/// - 结构体必须拥有 `instance: `[`j4rs::Instance`](j4rs::Instance)`,` 字段，且其余字段必须都是 [`std::marker::PhantomData`] 类型。
+/// - 枚举值则必须类似于此：
+///
+///   ```rust
+///   struct AType;
+///   struct BType;
+///   enum Enum{
+///     A(AType),
+///     #[fall] // 使用 `FromInstanceDerive` 时可选为分支添加 `fall` 属性。
+///     B(BType),
+///   }
+///   ```
+///
+///   并且如上代码，`AType` 和 `BType` 都必须实现 `FromInstanceTrait`.
+///   其中 `fall` 意味着未能成功转换的类型将会落到该分支中。如果没有该属性，未能成功转换时将会造成 `panic!`, 如果存在多个，则最后一个有效。
 #[proc_macro_derive(FromInstanceDerive, attributes(fall))]
 pub fn from_instance_derive(input: TokenStream) -> TokenStream {
     fn type_is_phantom_data(field: &Field) -> bool {
@@ -187,7 +229,19 @@ pub fn from_instance_derive(input: TokenStream) -> TokenStream {
     gen.into()
 }
 
-/// 不要求结构体有 `instance: Instance` 字段。
+/// ### `java_type`
+//
+/// 为结构体、枚举等实现 [`GetClassTypeTrait`](mj_base::env::GetClassTypeTrait).
+///
+/// 接受一个字符串字面值参数，类似于此：
+///
+/// ```rust
+/// use mj_macro::java_type;
+/// #[java_type("rt.lea.LumiaUtils")]
+/// struct LumiaUtils{}
+/// ```
+///
+/// 对结构体或枚举等没有特殊要求。
 #[proc_macro_attribute]
 pub fn java_type(type_name: TokenStream, input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
@@ -226,6 +280,12 @@ pub fn java_type(type_name: TokenStream, input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
+
+/// ### `mj_all`
+///
+/// 同时应用 [`GetInstanceDerive`], [`AsInstanceDerive`], [`FromInstanceDerive`] 和 [`java_type`].
+///
+/// 接受一个字符串字面值参数传递给 `java_type` 属性。
 #[proc_macro_attribute]
 pub fn mj_all(type_name: TokenStream, input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
@@ -237,7 +297,11 @@ pub fn mj_all(type_name: TokenStream, input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
-
+/// ### `MiraiEventDerive`
+///
+/// 为结构体和枚举类型实现 [`MiraiEventTrait`](mirai_j4rs::event::MiraiEventTrait).
+///
+/// 对结构体或枚举等没有特殊要求。`MiraiEventTrait` 特型会有部分要求，请参看 `mj_internal` 代码。
 #[proc_macro_derive(MiraiEventDerive)]
 pub fn mirai_event_derive(input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
@@ -250,6 +314,27 @@ pub fn mirai_event_derive(input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
+
+/// ### `mj_event`
+///
+/// 根据结构体名称应用 [`mj_all`] 和 [`MiraiEventDerive`]. 类似于此：
+///
+/// ```rust
+/// use mj_macro::mj_event;
+/// #[mj_event]
+/// pub struct FriendAddEvent {
+///     // Fields.
+/// }
+///
+/// // 相当于下述代码：
+/// // 这里的前缀是固定的。
+/// #[mj_macro::mj_all("net.mamoe.mirai.event.events.FriendAddEvent")]
+/// pub struct FriendAddEvent {
+///     // Fields.
+/// }
+/// ```
+///
+/// 也可以接受一个字符串字面值参数传递给 `mj_all` 属性，避免生成默认值。
 #[proc_macro_attribute]
 pub fn mj_event(mj_type: TokenStream, input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
@@ -266,6 +351,10 @@ pub fn mj_event(mj_type: TokenStream, input: TokenStream) -> TokenStream {
     };
     gen.into()
 }
+
+/// ### `mj_event_without_default_traits`
+///
+/// 与 [`mj_event`] 类似，只是没有应用 [`MiraiEventDerive`].
 #[proc_macro_attribute]
 pub fn mj_event_without_default_traits(mj_type: TokenStream, input: TokenStream) -> TokenStream {
     let ast: &DeriveInput = &syn::parse(input).unwrap();
